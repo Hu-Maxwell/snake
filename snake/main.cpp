34163a2a1aeaa15174f548e44bdb196fb954c7f4
dewe biag <dewebiag@gmail.com>
2024-09-11 22:27:46 -0700

    drew grid, edited class structure, recognizes key presses, updated TODO

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,56 +1,112 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-
 #include "snake.hpp"
+
+// TODO: 
+
+
+// implement snake moving around 
+// snake eating apple
+
+// apple respawning in random location
+// then add checks so apple does not spawn in snake
+
+// then add functionality for snake length increasing for one each apple eaten
+
+// make the snake's head different colored
+
+// if snake's head = any snake part, it dies
+
+// add start screen -> game -> freeze when die -> click -> start screen but different text   
+
 
 class Grid {
 public:
-
-    bool isEmpty;
-    bool isSnake;
-    bool isApple;
+    std::string arr[9][9];
 
     Grid() {
-        isEmpty = true; 
-    }
-
-    void fillStart(Grid (&arr)[9][9]) {
-        // puts snake and apple where they are
-        arr[2][5].isSnake = true; 
-        arr[6][5].isApple = true; 
-    }
-
-
-    void drawGrid() {
-        // assume 900 is width and height
-        // black screen, with very thin white lines for each one 
-        
         for (int i = 0; i < 9; i++) {
-            sf::RectangleShape(sf::Vector2f(1, 1));
-            // or use line func
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = "empty"; 
+            }
         }
+
+        arr[2][4] = "snake"; 
+        arr[6][4] = "apple";
+    }
+
+
+    void drawGrid(sf::RenderWindow& window) {
+
+        sf::Vector2u size = window.getSize();
+        unsigned int sideLen = size.x;
+        float boxLen = sideLen / 9.0f; 
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sf::RectangleShape square(sf::Vector2f(boxLen, boxLen)); 
+                square.setPosition(boxLen * j, boxLen * i); 
+
+                if (arr[j][i] == "empty") {
+                    square.setFillColor(sf::Color::Transparent);
+                    square.setOutlineThickness(1.f); 
+                }
+                else if (arr[j][i] == "snake") {
+                    square.setFillColor(sf::Color::Red);
+                }
+                else if (arr[j][i] == "apple") {
+                    square.setFillColor(sf::Color::Blue);
+                }
+
+                window.draw(square);
+
+            }
+        }
+    }
+
+    void moveSnake(sf::RenderWindow& window) {
+
     }
 }; 
 
 
-int main()
-{
-    Grid arr[9][9]; 
-
+int main() {
     sf::RenderWindow window(sf::VideoMode(900, 900), "snake");
+    window.setFramerateLimit(10); 
+
+    Grid grid; 
 
     while (window.isOpen()) {
+        window.clear();
+
+
 
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                std::cout << "W"; 
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                std::cout << "A";
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                std::cout << "S";
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                std::cout << "D";
+            }
+
         }
 
-        window.clear();
-        window.display(); 
 
+        // when key pressed change direction
+
+        grid.drawGrid(window); 
+        window.display(); 
     }
    
 }
