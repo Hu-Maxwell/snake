c9a3bf859270885efc6c17b0e8ff91376dc9cec5
dewe biag <dewebiag@gmail.com>
2024-09-24 22:04:23 -0700

    Actual final version. There are lots of things that still aren't done well, but I'm satisfied with the code I've written and what I've learnt.
    reword d9736b2 Final version. Seperated code from main.cpp into snake.cpp and snake.hpp for readability and organization.

--- snake/snake.hpp
+++ snake/snake.hpp
@@ -3,6 +3,30 @@
 
 #include <SFML/Graphics.hpp>
 
+class Node {
+public:
+    Node(int x, int y);
+};
+
+class SnakeList {
+public:
+    SnakeList();
+};
+
+class Grid {
+public:
+
+    Grid();
+    void respawnApple();
+    bool checkIfDead();
+    void updateSnake();
+    void drawGrid(sf::RenderWindow& window);
+    void moveSnake(sf::Keyboard::Key input);
+};
+
+void startScreen(sf::RenderWindow& window, bool& isPlaying); 
+void gameScreen(sf::RenderWindow& window, bool& isPlaying); 
+
 
 #endif
 
